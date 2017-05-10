57ab3acbb7f202be590b00ec121586dc7521f0dc
tkacleonid <tkacleonid@ya.ru>
2017-05-10 10:43:13 +0400

    First commit 7

--- src/adi.c
+++ src/adi.c
@@ -31,28 +31,28 @@ void xinvr()
         {            
             for (i = 1; i <= nx2; i++)
             {
-                ru1 = rho_i[k][j][i];
-                uu = us[k][j][i];
-                vv = vs[k][j][i];
-                ww = ws[k][j][i];
-                ac = speed[k][j][i];
+                ru1 = rho_i(k,j,i);
+                uu = us(k,j,i);
+                vv = vs(k,j,i);
+                ww = ws(k,j,i);
+                ac = speed(k,j,i);
                 ac2inv = ac*ac;
 
-                r1 = rhs[k][j][i][0];
-                r2 = rhs[k][j][i][1];
-                r3 = rhs[k][j][i][2];
-                r4 = rhs[k][j][i][3];
-                r5 = rhs[k][j][i][4];
+                r1 = rhs(k,j,i,0);
+                r2 = rhs(k,j,i,1);
+                r3 = rhs(k,j,i,2);
+                r4 = rhs(k,j,i,3);
+                r5 = rhs(k,j,i,4);
 
-                t1 = c2 / ac2inv * (qs[k][j][i] * r1 - uu*r2 - vv*r3 - ww*r4 + r5);
+                t1 = c2 / ac2inv * (qs(k,j,i) * r1 - uu*r2 - vv*r3 - ww*r4 + r5);
                 t2 = bt * ru1 * (uu * r1 - r2);
                 t3 = (bt * ru1 * ac) * t1;
 
-                rhs[k][j][i][0] = r1 - t1;
-                rhs[k][j][i][1] = -ru1 * (ww*r1 - r4);
-                rhs[k][j][i][2] = ru1 * (vv*r1 - r3);
-                rhs[k][j][i][3] = -t2 + t3;
-                rhs[k][j][i][4] = t2 + t3;
+                rhs(k,j,i,0) = r1 - t1;
+                rhs(k,j,i,1) = -ru1 * (ww*r1 - r4);
+                rhs(k,j,i,2) = ru1 * (vv*r1 - r3);
+                rhs(k,j,i,3) = -t2 + t3;
+                rhs(k,j,i,4) = t2 + t3;
             }
         }
     }
@@ -69,7 +69,7 @@ void add2()
         for (j = 1; j <= ny2; j++) {
             for (i = 1; i <= nx2; i++) {
                 for (m = 0; m < 5; m++) {
-                    u[k][j][i][m] = u[k][j][i][m] + rhs[k][j][i][m];
+                    u(k,j,i,m) = u(k,j,i,m) + rhs(k,j,i,m);
                 }
             }
         }
